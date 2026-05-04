a07dd04b583f8e0fd82daf0dd4fd487c32c5f07a
renato <renat.asaev.92@mail.ru>
2026-05-04 22:19:08 +0300

    first commit

--- src/controllers/publisher.controller.js
+++ src/controllers/publisher.controller.js
@@ -1,12 +1,20 @@
-import {Author} from "../model/index.js";
+import {Author, Book} from "../model/index.js";
 
 export const findPublishersByAuthor = async (req, res) => {
     const author = await Author.findByPk(req.params.name);
     if (!author) {
         return res.status(404).json({error: `Author with name ${req.params.name} not found`});
     }
-    const books = await author.getBooks();
-    const publishers = [...new Set(books.map(book => book.publisher))];
-    return res.json(publishers);
-
+    const publishers = await Book.aggregate('publisher', 'DISTINCT', {
+        plain: false,       // тип запроса SELECT и возвращает одну строку
+        include: {
+            model: Author,
+            as: 'authors',
+            where: {name: req.params.name},
+            through: {
+                attributes: []
+            }
+        }
+    })
+    return res.json(publishers.map(p => p.DISTINCT));
 }
